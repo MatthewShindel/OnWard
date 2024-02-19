a3d66da6004c6a42934bc913a628bade0370baa3
William Lavery <lavery_william@yahoo.com>
2024-02-18 22:26:10 -0500

    add changes to ResultPage test

--- src/components/ResultPage/ResultPage.test.js
+++ src/components/ResultPage/ResultPage.test.js
@@ -1,23 +1,63 @@
 import { render, fireEvent } from "@testing-library/react";
 import ResultPage from "./ResultPage";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe(ResultPage, () => {
   it("renders the text in each header", () => {
-    const { getByText } = render(<ResultPage />);
+    const salary =  100000;
+    const expenses =  20000;
+    const zipcode = '12345';
+    const combinedRate =  0.1;
+    const { getByText } = render(
+      <Router>
+        <ResultPage 
+          salary={salary} 
+          expenses={expenses} 
+          zipcode={zipcode} 
+          combinedRate={combinedRate}
+        />
+      </Router>
+    );
     expect(getByText('This is the salary after taxes,')).toBeInTheDocument();
-
   })
 });
-// describe('Form', () => {
-// 	it('renders all input fields and submit button', () => {
-//     const { getByPlaceholderText, getByText } = render(<Form />);
-    
-//     expect(getByPlaceholderText('Salary')).toBeInTheDocument();
-//     expect(getByPlaceholderText('Expenses')).toBeInTheDocument();
-//     expect(getByPlaceholderText('Zipcode')).toBeInTheDocument();
-//     expect(getByText('SUBMIT')).toBeInTheDocument();
-//   });
-// })
+
+// describe(ResultPage, () => {
+//   it("renders the text in each header", () => {
+//     // Define the variables with some example values
+//     const salary =  50000;
+//     const expenses =  20000;
+//     const zipcode = '12345';
+//     const combinedRate =  0.1;
+
+//     const { getByText } = render(
+//       <ResultPage  
+//         salary={salary}  
+//         expenses={expenses}  
+//         zipcode={zipcode}  
+//         combinedRate={combinedRate}
+//       />
+//     );
+//     expect(getByText('This is the salary after taxes,')).toBeInTheDocument();
+//   })
+// });
+
+// it('submits the form with valid data', () => {
+//   const updateSalary = jest.fn();
+//   const updateExpenses = jest.fn();
+//   const updateZipcode = jest.fn();
+//   const updateCombinedRate = jest.fn();
+//   const navigate = jest.fn();
+
+//   const { getByPlaceholderText, getByText } = render(
+//     <Form
+//       updateSalary={updateSalary}
+//       updateExpenses={updateExpenses}
+//       updateZipcode={updateZipcode}
+//       updateCombinedRate={updateCombinedRate}
+//       navigate={navigate}
+//     />
+//   );
 // 82947
 {/* <h2>This is the salary, ${salary}</h2>
 			<h2>This is the salary after taxes, ${salaryAfterTax}</h2>
