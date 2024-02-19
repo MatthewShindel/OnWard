685baff7544448bb93bf435121b1588979f086c1
MatthewShindel <matthew@shindel.com>
2024-02-19 14:37:33 -0500

    add testing body for  Form.test and ErrorPage.Test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,12 +6,15 @@ import ErrorPage from '../ErrorPage/ErrorPage.js';
 import ResultPage from '../ResultPage/ResultPage.js';
 import Form from '../Form/Form.js';
 import { getTaxRate } from '../../ApiCalls.js';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
 	const [zipcode, setZipcode] = useState(0);
 	const [salary, setSalary] = useState(0);
 	const [expenses, setExpenses] = useState(0);
 	const [combinedRate, setCombinedRate] = useState(0);
+	const navigate = useNavigate();
+
 
 	function updateZipcode(zipcode) {
 		setZipcode(zipcode)
@@ -48,7 +51,7 @@ function App() {
 
 			<main>
 				<Routes>
-					<Route path='/' element={<Form updateSalary = {updateSalary} updateZipcode = {updateZipcode}  updateExpenses = {updateExpenses} updateCombinedRate = {updateCombinedRate}/>}/>
+					<Route path='/' element={<Form updateSalary = {updateSalary} updateZipcode = {updateZipcode}  updateExpenses = {updateExpenses} updateCombinedRate = {updateCombinedRate} navigate = {navigate}/>}/>
 					<Route path='/result' element={<ResultPage zipcode = {zipcode} salary = {salary} expenses = {expenses} combinedRate = {combinedRate}/>}/>
 					<Route path='*' element={<ErrorPage/>}/>
 				</Routes>
