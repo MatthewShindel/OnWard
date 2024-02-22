3c85cd8a25a786a53689914bd70a0e814e6d97f3
MatthewShindel <matthew@shindel.com>
2024-02-22 12:11:05 -0500

    update ResultPage.test.js to match our new ResultPage.js

--- src/components/ResultPage/ResultPage.test.js
+++ src/components/ResultPage/ResultPage.test.js
@@ -1,35 +1,34 @@
-import { render, fireEvent } from "@testing-library/react";
-import ResultPage from "./ResultPage";
+import React from 'react';
+import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ResultPage from './ResultPage';
 
-describe(ResultPage, () => {
-  it("renders the text in each header, renders the salary, salary after taxes, annual expenses, tax on expenses, zipcode, and salary after tax and expenses", () => {
-    const salary =  100000;
-    const expenses =  20000;
-    const zipcode = '12345';
-    const combinedRate =  0.1;
-    const expectedSalaryAfterTaxes = 82947;
-    const expensesTax = 2000;
-    const salaryAfterTaxAndExpenses = 80947;
-    const { getByText } = render(
-      <Router>
-        <ResultPage 
-          salary={salary} 
-          expenses={expenses} 
-          zipcode={zipcode} 
-          combinedRate={combinedRate}
-        />
-      </Router>
-    );
-    expect(getByText(/This is the ResultPage/)).toBeInTheDocument();
-    expect(getByText(new RegExp(`This is the salary, \\$${salary}`))).toBeInTheDocument();
-    expect(getByText(new RegExp(`This is the salary after taxes, \\$${expectedSalaryAfterTaxes}`))).toBeInTheDocument();
-    expect(getByText(new RegExp(`This is the annual expenses, \\$${expenses}`))).toBeInTheDocument();
-    expect(getByText(new RegExp(`This is the tax on expenses, \\$${expensesTax}`))).toBeInTheDocument();
-    expect(getByText(new RegExp(`This is the zipcode, ${zipcode}`))).toBeInTheDocument();
-    expect(getByText(/This is your final salary after all expenses and taxes:/)).toBeInTheDocument();
-    expect(getByText(`\$${salaryAfterTaxAndExpenses}`)).toBeInTheDocument();
-    expect(getByText(/Click here to head back home!/)).toBeInTheDocument();
-    
-  })
+test('renders correct content in ResultPage component', () => {
+  const salary = 100000;
+  const expenses = 2000;
+  const zipcode = 12345;
+  const combinedRate = 0.01;
+
+  const { getByText } = render(
+    <Router> {}
+      <ResultPage
+        salary={salary}
+        expenses={expenses}
+        zipcode={zipcode}
+        combinedRate={combinedRate}
+      />
+    </Router>
+  );
+
+  const baseSalaryText = getByText(`Base Salary: $100,000`);
+  const salaryAfterTaxText = getByText('Salary after income Tax: $82,947');
+  const expensesText = getByText('Expenses: -$24,000');
+  const expensesTaxText = getByText('Expenses Taxes: -$240');
+  const finalSalaryText = getByText('Final Salary: $58,707');
+
+  expect(baseSalaryText).toBeInTheDocument();
+  expect(salaryAfterTaxText).toBeInTheDocument();
+  expect(expensesText).toBeInTheDocument();
+  expect(expensesTaxText).toBeInTheDocument();
+  expect(finalSalaryText).toBeInTheDocument();
 });
